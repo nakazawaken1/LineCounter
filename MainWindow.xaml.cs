d64ddb333c937bc0cd5887c078db820bf728d8dd
NAKAZAWA Kenichi <k.nakazawa@systemsquare.co.jp>
2014-03-25 15:16:55 +0900

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LineCounter
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ObservableCollection<string> Paths = new ObservableCollection<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ListBoxPaths_DoubleClick(object o, EventArgs e)
+        {
+            Paths.Remove(((ListBoxItem)o).Content as string).ToString();
+        }
+
+        private void ListBoxPaths_Drop(object sender, DragEventArgs e)
+        {
+            foreach (var i in e.Data.GetData(DataFormats.FileDrop) as string[]) if (!Paths.Contains(i)) Paths.Add(i);
+        }
+
+        private void ListBoxPaths_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = e.Data.GetData(DataFormats.FileDrop) != null ? DragDropEffects.Copy : DragDropEffects.None;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch(((Button)sender).Content as string)
+            {
+                case "行カウント開始":
+                    if (Paths.Count <= 0)
+                    {
+                        MessageBox.Show("行数を数えるファイルまたはフォルダを中央にドラッグしてください。");
+                        return;
+                    }
+                    var result = App.Current.Windows.OfType<ResultWindow>().FirstOrDefault();
+                    if (result == null) result = new ResultWindow();
+                    result.Owner = this;
+                    result.Title = result.Setup(ResultWindow.Rows, Paths, TextBoxExclude.Text.Split(new char[] { ' ', '　' }, StringSplitOptions.RemoveEmptyEntries));
+                    result.ShowDialog();
+                    break;
+                case "一覧クリア":
+                    Paths.Clear();
+                    break;
+                default:
+                    Close();
+                    break;
+            }
+        }
+    }
+}
---
+++ ResultWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LineCounter
+{
+    /// <summary>
+    /// ResultWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ResultWindow : Window
+    {
+        public static ObservableCollection<Row> Rows = new ObservableCollection<Row>();
+        public ResultWindow()
+        {
+            InitializeComponent();
+        }
+        public string Setup(ObservableCollection<Row> rows, IEnumerable<string> list, IEnumerable<string> exclude, bool first = true)
+        {
+            if (first) rows.Clear();
+            foreach (var i in list)
+            {
+                if (exclude.Any(j => i.EndsWith(j, StringComparison.CurrentCultureIgnoreCase))) continue;
+                if (System.IO.Directory.Exists(i))
+                {
+                    Setup(rows, System.IO.Directory.EnumerateFileSystemEntries(i), exclude, false);
+                }
+                else if(!rows.Any(j => j.Path == i))
+                {
+                    rows.Add(new Row(i));
+                }
+            }
+            return string.Format("全 {0:#,0}ファイル {1:#,0}行", rows.Count, rows.Aggregate(0L, (s, r) => s += r.Count));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Content as string)
+            {
+                case "コピー":
+                    Clipboard.SetText(ToText(Rows, "\t"));
+                    break;
+                case "CSVで保存":
+                    var dialog = new SaveFileDialog { FileName = "line_count.tsv", DefaultExt=".csv", Filter = "CSV形式 (.csv)|*.csv" };
+                    if (dialog.ShowDialog() ?? false) System.IO.File.WriteAllText(dialog.FileName, ToText(Rows, ","));
+                    break;
+                default:
+                    Close();
+                    break;
+            }
+        }
+        public static string ToText(IEnumerable<Row> rows, string separator)
+        {
+            var text = new StringBuilder();
+            foreach (var i in Rows)
+            {
+                text.Append(i.Path).Append(separator).Append(i.Type).Append(separator).AppendLine(i.Count.ToString());
+            }
+            return text.ToString();
+        }
+
+        private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(((Row)((DataGridRow)sender).Item).Path);
+        }
+    }
+    public class Row
+    {
+        public Row(string path)
+        {
+            Path = path;
+            Type = System.IO.Path.GetExtension(path);
+            Count = System.IO.File.ReadAllLines(path).Length;
+        }
+        public string Path { get; set; }
+        public string Type { get; set; }
+        public long Count { get; set; }
+    }
+}
